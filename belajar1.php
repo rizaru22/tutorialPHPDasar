890fbbc9cebdaea44ffbfbd305f289e0b0272960
rizaru22 <rizaru.2.2@gmail.com>
2023-01-26 05:46:00 +0700

    tutorial 1

---
+++ belajar1.php
@@ -0,0 +1,21 @@
+<?php
+$nama="Anto";
+// cara menampilkan teks dengan Echo
+echo "<h1>Selamat Datang Di Tutorial PHP Dasar</h1>";
+echo "Hai Budi"," selamat mengikuti Tutorial <br>";
+echo "Hai ",$nama ," selamat mengikuti Tutorial <br>";
+
+//cara menampilkan teks dengan print
+$cetak=print "Belajar PHP Dasar untuk pemula <br>";
+
+
+//cara menampilkan teks dengan printf
+$teks="PHP";
+printf (" Belajar %s Menyenangkan",$teks);
+$uang=2000;
+printf ("Andi memiliki uang sebesar %.2f",$uang);
+
+
+
+
+
